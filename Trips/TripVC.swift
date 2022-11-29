c7e9b307416df8f445e72d6a7459045a79c8198d
Egor Syrtcov <sev_84@inbox.ru>
2022-11-29 16:50:48 +0300

    Starting project

---
+++ Trips/TripVC.swift
@@ -0,0 +1,18 @@
+//
+//  TripVC.swift
+//  Trips
+//
+//  Created by Egor Syrtcov on 29.11.22.
+//
+
+import UIKit
+
+class TripVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .white
+    }
+}
+
